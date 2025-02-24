108fb689ca9bfe6e6605b6b769cb980f26c96978
ahmed hafez <ahmedhafez1174@gmail.com>
2025-02-24 21:22:06 +0200

    order server url

--- src/app/core/services/orders/orders.service.ts
+++ src/app/core/services/orders/orders.service.ts
@@ -12,7 +12,7 @@ export class OrdersService {
 
 
   checkoutPayment(id:string , data:object):Observable<any>{
-    return this.httpClient.post(environment.baseUrl+`/api/v1/orders/checkout-session/${id}?url=http://localhost:4200`,
+    return this.httpClient.post(environment.baseUrl+`/api/v1/orders/checkout-session/${id}?url=https://ng-ecommerce-ten.vercel.app/`,
       {
         "shippingAddress":data
       },
